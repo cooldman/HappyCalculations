a0bc3bc709c28def61e22d5ef539f302368a4b59
y.zakharov <y.zakharov@iis-soft.ru>
2015-05-22 10:48:09 +0300

    Подсчет результатов

--- app/src/main/java/com/onedeveloperstudio/happycalculations/MainActivity.java
+++ app/src/main/java/com/onedeveloperstudio/happycalculations/MainActivity.java
@@ -3,49 +3,92 @@ package com.onedeveloperstudio.happycalculations;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * y.zakharov on 22.04.2015.
  */
 public class MainActivity extends Activity {
-  private TextView firstNumber;
-  private TextView secondNumber;
-  private TextView operator;
+  private TextView firstNumberField;
+  private TextView secondNumberField;
+  private TextView operatorField;
   private Integer maxValue;
   private Operations operation;
+  private Button checkButton;
+  private EditText resultField;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main_activity);
-    firstNumber = (TextView) findViewById(R.id.first);
-    secondNumber = (TextView) findViewById(R.id.second);
-    operator = (TextView) findViewById(R.id.operator);
+    firstNumberField = (TextView) findViewById(R.id.first);
+    secondNumberField = (TextView) findViewById(R.id.second);
+    operatorField = (TextView) findViewById(R.id.operator);
+    resultField = (EditText) findViewById(R.id.result);
     Intent intent = getIntent();
-    Operations operation = (Operations) intent.getSerializableExtra("operation");
+    final Operations operation = (Operations) intent.getSerializableExtra("operation");
     this.operation = operation;
     prepareForOperation(operation);
-    firstNumber.setText(maxValue.toString());
-    secondNumber.setText(String.valueOf(maxValue/2));
+    firstNumberField.setText(maxValue.toString());
+    secondNumberField.setText(String.valueOf(maxValue / 2));
+    checkButton = (Button) findViewById(R.id.checkButton);
+    checkButton.setOnClickListener(new View.OnClickListener() {
+                                     @Override
+                                     public void onClick(View v) {
+                                       if (!resultField.getText().equals("")) {
+                                         Integer result = getIntegerValueOfTextField(resultField);
+                                         Integer firstNumber = getIntegerValueOfTextField(firstNumberField);
+                                         Integer secondNumber = getIntegerValueOfTextField(secondNumberField);
+                                         switch (operation) {
+                                           case PLUS:
+                                               Toast.makeText(getApplicationContext(), String.valueOf(result.equals(firstNumber + secondNumber)), Toast.LENGTH_LONG).show();
+                                             break;
+                                           case MULTIPLY:
+                                             Toast.makeText(getApplicationContext(),  String.valueOf(result.equals(firstNumber * secondNumber)), Toast.LENGTH_LONG).show();
+                                             break;
+                                           case DIVISION:
+                                             Toast.makeText(getApplicationContext(),  String.valueOf(result.equals(firstNumber / secondNumber)), Toast.LENGTH_LONG).show();
+                                             break;
+                                           case POWER:
+                                             Toast.makeText(getApplicationContext(),  String.valueOf(result.equals((int)Math.pow(firstNumber, secondNumber))), Toast.LENGTH_LONG).show();
+                                             break;
+                                         }
+                                       } else {
+                                         Toast.makeText(getApplicationContext(), "Необходимо заполнить значение в поле Результат", Toast.LENGTH_LONG).show();
+                                       }
+                                     }
+                                   }
+    );
   }
 
-  private void prepareForOperation(Operations operation){
+  private Integer getIntegerValueOfTextField(TextView textView) {
+    return Integer.valueOf(String.valueOf(textView.getText()));
+  }
+
+  private Integer getIntegerValueOfTextField(EditText textView) {
+    return Integer.valueOf(String.valueOf(textView.getText()));
+  }
+
+  private void prepareForOperation(Operations operation) {
     switch (operation) {
       case PLUS:
-        operator.setText("+");
+        operatorField.setText("+");
         maxValue = 50;
         break;
       case MULTIPLY:
-        operator.setText("*");
+        operatorField.setText("*");
         maxValue = 10;
         break;
       case DIVISION:
-        operator.setText("/");
+        operatorField.setText("/");
         maxValue = 100;
         break;
       case POWER:
-        operator.setText("pow");
+        operatorField.setText("pow");
         maxValue = 10;
         break;
     }
